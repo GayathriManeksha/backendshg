58139a7084f739e2da6ac1d406d138695fe8d9d2
GayathriManeksha <gaya3maneksha@github.com>
2023-06-18 01:08:32 +0530

    Combined fns

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -36,9 +36,29 @@ router.post('/makepayment', async (req, res) => {
 
         unit.payments.push(newpayment);
         await unit.save();
+
         const paymentId = unit.payments[unit.payments.length - 1]._id;
-        console.log("Saved successfully",paymentId)
-        return res.json({ status: true, paymentId });
+        console.log("Saved successfully", paymentId)
+
+        const desc = `${user2.name} paid ${amt}`;
+        console.log(desc)
+
+        const proposal = {
+            description: desc,
+            votes: 0,
+            approved: false,
+            typeproposal: 'Payment',
+            toapprove: paymentId,
+            datecreated: new Date(),
+        };
+
+        console.log(proposal)
+        // Save the proposal to the database
+        unit.proposals.push(proposal);
+        await unit.save();
+
+        res.status(201).json(proposal);
+        // return res.json({ status: true, paymentId });
     }
     catch (error) {
         console.log("error")
